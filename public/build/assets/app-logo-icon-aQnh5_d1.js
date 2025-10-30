846ddd53650defa4fe1fa77db91bc4659af1d25a
rhadityabwo <rhadityabwo@gmail.com>
2025-10-30 13:59:39 +0700

    add built assets for deployment

---
+++ public/build/assets/app-logo-icon-aQnh5_d1.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-DpJz9MDM.js";function a(o){return s.jsx("img",{...o,src:"/logo-ieb.png",alt:"Logo IEB",className:`object-contain ${o.className||""}`})}export{a as A};
--- public/build/assets/input-error-BcjaqAnj.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BK7dJVUQ.js";import{a as o}from"./createLucideIcon-BwJ_09SO.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
