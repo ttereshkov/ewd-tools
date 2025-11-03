476f0082204702886ce5fe5aaec651ed6f4c8a07
rhadityabwo <rhadityabwo@gmail.com>
2025-11-03 15:58:17 +0700

    fix

--- public/build/assets/input-error-C0Qw0dez.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-UL9fVeUY.js";import{a as o}from"./createLucideIcon-BFFAIQkP.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
